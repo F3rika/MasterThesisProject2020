65b942028b99bcfb09ed6d4547e96e74acfbb396
F3rika <79254117+F3rika@users.noreply.github.com>
2021-02-20 12:02:40 +0100

    Update vcf_manual_DP_filtering.py

--- Python3/vcf_manual_DP_filtering.py
+++ Python3/vcf_manual_DP_filtering.py
@@ -1,5 +1,14 @@
-#The following python3 script was produced to be specifically performed on
-#my samples and needs improvement to be applied in a more "general" way
+#Variant calling results (VCF file fomat) were filtered for read depth (DP) by the means of a custom Python3 script, which functions as follows.
+#Briefly, each VCF file is read line by line taking in account only the FORMAT and VALUE fields.
+#The split function in Python is used to separate the values constituting these fields in their single elements and results are saved in two distinct
+#list objects.
+#The program then scans the list associated with the FORMAT field start to end, and stops when the index associated with the DP tag is identified.
+#The corresponding value is extracted from the list of items contained in the VALUE field list. This is possible since tags in the FORMAT field and 
+#corresponding values in the VALUE field have a one to one correspondence in a VCF file.
+#All the variants with DP equal or higher than 10 are reported in the output VCF file.
+
+#Note that the following python3 script was produced to be specifically performed on
+#my samples and needs improvement to be applied in a more "general" way.
 
 samples=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 tools=['freebayes','varscan','gatk']
